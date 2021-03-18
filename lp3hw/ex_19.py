88f665573cbfe25744f4b11b24701d1f08c068cc
srapisardi <50967041+srapisardi@users.noreply.github.com>
2021-03-18 14:48:07 -0400

    Add files via upload

---
+++ lp3hw/ex_19.py
@@ -0,0 +1,18 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print("You have",cheese_count,"cheeses!")
+    print("you have",boxes_of_crackers,"crackers!")
+
+# Integers as arguments
+cheese_and_crackers(20,30)
+
+cheese = 10
+crackers = 50
+
+# Variables as arguments
+cheese_and_crackers(cheese, crackers)
+
+# Math as arguments
+cheese_and_crackers(10 + 5, 20 + 20)
+
+# Math using variables
+cheese_and_crackers(cheese + 20, crackers + 15)
