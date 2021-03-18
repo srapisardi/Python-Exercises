e17854e6e7ed1b460ec27f7796e3709586f42d19
srapisardi <50967041+srapisardi@users.noreply.github.com>
2021-03-18 15:04:46 -0400

    Add files via upload

---
+++ lp3hw/ex_21.py
@@ -0,0 +1,20 @@
+def add(a,b):
+    sum = a + b
+    return sum
+
+def subtract(a,b):
+    difference = a - b
+    return difference
+
+def multiply(a,b):
+    product = a * b
+    return product
+
+def divide(a,b):
+    quotient = a / b
+    return quotient
+
+print(add(1,2))
+print(subtract(5,2))
+print(multiply(6,6))
+print(divide(25,5))
